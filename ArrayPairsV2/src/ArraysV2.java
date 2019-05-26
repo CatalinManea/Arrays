4d0c33d6b81b504d73f78e1b884f025894eaa446
CatalinManea <51023560+CatalinManea@users.noreply.github.com>
2019-05-26 12:36:55 +0300

    Add files via upload

---
+++ ArrayPairsV2/src/ArraysV2.java
@@ -0,0 +1,28 @@
+
+public class ArraysV2 {
+	
+	public static void main(String[] args)
+	{
+		int [] Array_A = {4,5,10,11,6,7,100,99};
+		int M=-1,i=1;
+		
+		System.out.println("All the pairs of numbers that,if subtracted from the Array_A"
+				+ " are equal with "+M +", are : ");
+		
+		for(int item1 : Array_A) 
+		{
+			
+			for(int item2 : Array_A) 
+			{
+				
+				if((item1-item2)==M) 
+				{				
+					
+					System.out.println("Pair " +i + ": "+ item1 + "-" + item2+"="+M);
+					i++;
+					
+				}				
+			}
+		}
+	}
+}
